e467803b934ac4418ec45e1fc6d09394c9be1694
aruncjohn44 <aruncjohn@gmail.com>
2025-11-15 10:32:51 +0530

    added operations and unit tests

---
+++ tests/test_operations.py
@@ -0,0 +1,13 @@
+from src.math_ops import addition, subtraction
+
+
+
+def test_addition():
+    assert addition(2, 3) == 5
+    assert addition(-1, 1) == 0
+    assert addition(0, 0) == 0
+
+def test_subtraction():
+    assert subtraction(5, 3) == 2
+    assert subtraction(0, 0) == 0
+    assert subtraction(-1, -1) == 0
